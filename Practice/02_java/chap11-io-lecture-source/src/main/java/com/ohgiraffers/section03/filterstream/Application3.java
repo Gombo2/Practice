684a349ad47f30d3aabc13a6c4577ac1a7d04848
Gombo2 <rhtjddus0502@gmail.com>
2025-02-07 09:50:07 +0900

    DataInput,output 보토스트림/필터스트림 관련 예제 작성 및 개인정리 주석 추가

---
+++ Practice/02_java/chap11-io-lecture-source/src/main/java/com/ohgiraffers/section03/filterstream/Application3.java
@@ -0,0 +1,32 @@
+package com.ohgiraffers.section03.filterstream;
+
+import java.io.DataOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Application3 {
+    public static void main(String[] args) {
+
+        /* 수업목표. 데이터 타입 입출력 보조 스트림을 이해하고 활용할 수 있다. */
+        DataOutputStream dos = null;
+        try {
+            //int,double,float 단위등 타입단위로 쏠 수 있음, 그대신 읽을때도 int,dobule,float 순으로 받아야함
+            dos = new DataOutputStream(
+                    new FileOutputStream(
+                            "src/main/java/com/ohgiraffers/section03/filterstream/testData.txt"
+                    )
+            );
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (dos != null) {
+                    dos.close();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
--- Practice/Algorithm/src/main/java/safetyArea.java
+++ Practice/Algorithm/src/main/java/safetyArea.java
@@ -10,15 +10,35 @@ public class safetyArea {
         StringTokenizer st;
         int n = Integer.parseInt(br.readLine());
         int[][] arr = new int[n][n];
+
         for(int i = 0; i < arr.length; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < arr[i].length; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
-//            System.out.println(Arrays.toString(arr[i]));
         }
-        for(int i = 0; i < arr.length; i++) {
-            System.out.println(Arrays.toString(arr[i]));
+
+        int max = getMax(arr);
+
+        for(int j = 1; j <= max; j++) {
+
         }
     }
+
+    private static int getMax(int[][] arr) {
+        int max = 0;
+
+        for(int i : arr) {
+            if(i > max) {
+                max = i;
+            }
+        }
+
+        return max;
+    }
+
+    private static boolean isOver(int rain, int area) {
+        return rain >= area;
+    }
+
 }
