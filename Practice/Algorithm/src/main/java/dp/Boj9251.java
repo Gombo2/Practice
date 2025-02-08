10dc6a9141ba6d6b230cf044ec7ebda389073e1a
Gombo2 <rhtjddus0502@gmail.com>
2025-02-08 17:48:10 +0900

    백준 문제풀이

--- Practice/Algorithm/src/main/java/dp/Boj9251.java
+++ Practice/Algorithm/src/main/java/dp/Boj9251.java
@@ -3,47 +3,25 @@ package dp;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
 
 public class Boj9251 {
     public static void main(String[] args) throws IOException {
-    String[] words = new String[2];
-    Map<String,Integer> word1 = new HashMap<>();
-    Map<String,Integer> word2 = new HashMap<>();
-
-    words = initializationString(words);
-    word1 = getMap(words[0]);
-    word2 = getMap(words[1]);
-
-
-    }
-
-    private static String[] initializationString(String[] words) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        words[0] = st.nextToken();
-        st = new StringTokenizer(br.readLine());
-        words[1] = st.nextToken();
+        String str1 = br.readLine();
+        String str2 = br.readLine();
 
-        return words;
-    }
+        int[][] dp = new int[str1.length() + 1][str2.length() + 1];
 
-    private static Map<String,Integer> getMap(String word) {
-        Map<String, Integer> eachCountMap = new HashMap<>();
-        String[] splited = word.split("");
-
-        for (int i = 0; i < splited.length; i++) {
-            if(eachCountMap.get(splited[i]) == null) {
-                eachCountMap.put(splited[i], 0);
-            } else {
-                eachCountMap.put(splited[i], eachCountMap.get(splited[i]) + 1);
+        for (int i = 1; i <= str1.length(); i++) {
+            for (int j = 1; j <= str2.length(); j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
             }
-
         }
 
-        return eachCountMap;
+        System.out.println(dp[str1.length()][str2.length()]);
     }
 }
