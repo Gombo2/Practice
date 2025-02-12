13b9a4a551af1c04e87a2ec8bc026f2b50dee88e
Gombo2 <rhtjddus0502@gmail.com>
2025-02-12 11:00:35 +0900

    section04 수정

--- Practice/02_java/chap11-io-lecture-source/src/main/java/com/ohgiraffers/section04/testapp/run/Application.java
+++ Practice/02_java/chap11-io-lecture-source/src/main/java/com/ohgiraffers/section04/testapp/run/Application.java
@@ -1,5 +1,7 @@
 package com.ohgiraffers.section04.testapp.run;
 
+import com.ohgiraffers.section04.testapp.aggregate.BloodType;
+import com.ohgiraffers.section04.testapp.aggregate.Member;
 import com.ohgiraffers.section04.testapp.service.MemberService;
 
 import java.util.Scanner;
@@ -25,8 +27,11 @@ public class Application {
             switch (input) {
                 case 1: ms.findAllMembers(); break;
                 case 2: ms.findMemberBy(chooseMemNo()); break;
-                case 3: break;
-                case 4: break;
+                case 3: ms.registMember(signUp()); break;
+                case 4:
+                    Member selected = ms.findMemberForMod(chooseMemNo());
+
+                        break;
                 case 5: break;
                 case 9:
                     System.out.println("회원관리 프로그램을 종료합니다."); return;
@@ -34,6 +39,45 @@ public class Application {
                     System.out.println("번호를 잘못 입력하셨습니다.");
             }
         }
+    }
+
+    private static Member signUp() {
+        Member member = null;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("아이디를 입력하세요: ");
+        String id = sc.nextLine();
+
+        System.out.print("패스워드를 입력하세요: ");
+        String pwd = sc.nextLine();
+
+        System.out.print("나이를 입력하세요: ");
+        int age = sc.nextInt();
+
+        System.out.print("입력할 취미의 갯수를 입력하세요(숫자는 1 이상): ");
+        int length = sc.nextInt();
+        sc.nextLine();      // 버퍼에 남은 엔터 제거용
+
+        String[] hobbies = new String[length];
+        for (int i = 0; i < hobbies.length; i++) {
+            System.out.print((i + 1) + "번째 취미를 입력하세요: ");
+            String input = sc.nextLine();
+            hobbies[i] = input;
+        }
+
+        System.out.print("혈액형을 입력하세요(A, AB, B, O): ");
+        String bloodType = sc.nextLine().toUpperCase();
+        BloodType bt = null;
+        switch (bloodType) {
+            case "A": bt = BloodType.A; break;
+            case "AB": bt = BloodType.AB; break;
+            case "B": bt = BloodType.B; break;
+            case "O": bt = BloodType.O; break;
+        }
+
+        member = new Member(id, pwd, age, hobbies, bt);
+
+        return member;
     }
 
     /* 설명. 회원 번호를 입력 받아 int로 반환하는 메소드 */
