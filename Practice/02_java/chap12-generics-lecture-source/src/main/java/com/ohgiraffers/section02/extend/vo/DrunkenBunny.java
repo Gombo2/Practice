676a2a9e5a29c92009a4846ea5c054eebc7556ac
Gombo2 <rhtjddus0502@gmail.com>
2025-02-18 23:21:12 +0900

    파일 뒤집어넣기

--- Practice/02_java/chap12-generics-lecture-source/src/main/java/com/ohgiraffers/section02/extend/vo/DrunkenBunny.java
+++
@@ -1,8 +0,0 @@
-package com.ohgiraffers.section02.extend.vo;
-
-public class DrunkenBunny extends Rabbit {
-    @Override
-    public void cry() {
-        System.out.println("봐니봐니 다!@당근 당그@!@은");
-    }
-}
