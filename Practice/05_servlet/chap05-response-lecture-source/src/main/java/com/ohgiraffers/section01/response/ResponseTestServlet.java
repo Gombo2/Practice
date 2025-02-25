80521c149afbe90dfa74805a2bf53e0bb029a91c
Gombo2 <rhtjddus0502@gmail.com>
2025-02-25 15:59:32 +0900

    ㅇ

--- Practice/05_servlet/chap05-response-lecture-source/src/main/java/com/ohgiraffers/section01/response/ResponseTestServlet.java
+++ Practice/05_servlet/chap05-response-lecture-source/src/main/java/com/ohgiraffers/section01/response/ResponseTestServlet.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/response")
+ @WebServlet("/response")
 public class ResponseTestServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
