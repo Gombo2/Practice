57b8c3632811cb3e7ad32844b83dbca3e0a48d69
Gombo2 <rhtjddus0502@gmail.com>
2025-02-25 15:23:48 +0900

    session 예제

---
+++ Practice/05_servlet/chap10-session-lecture-source/src/main/java/com/ohgiraffers/session/RedirectServlet.java
@@ -0,0 +1,27 @@
+package com.ohgiraffers.session;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet("/redirect")
+public class RedirectServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+
+        String firstName = session.getAttribute("firstName").toString();  // 동적 바인딩에 의해 둘다 됨.
+        String lastName = session.getAttribute("lastName").toString();  // 동적 바인딩에 의해 둘다 됨.
+//        String firstName = (String) session.getAttribute("firstName");
+//        String lastName = (String) session.getAttribute("lastName");
+
+        System.out.println("firstName = " + firstName);
+        System.out.println("lastName = " + lastName);
+    }
+}
