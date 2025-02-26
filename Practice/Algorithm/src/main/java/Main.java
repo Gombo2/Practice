3f80de2c340efcd493abe13e178cc5d1aa188d80
Gombo2 <rhtjddus0502@gmail.com>
2025-02-26 17:36:30 +0900

    2583 문제 풀이

--- Practice/Algorithm/src/main/java/Main.java
+++ Practice/Algorithm/src/main/java/Main.java
@@ -1,81 +1,94 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 
 public class Main {
     static int[][] map;
-    static boolean[][] visit;
-    static int worms = 0;
-    static Queue<Node> que;
+    static boolean[][] visited;
+    static int countSize;
+    static List<Integer> countSizeList;
     static int[] moveX = {0, 0, -1, 1};
     static int[] moveY = {1, -1, 0, 0};
-    static int curX;
-    static int curY;
 
-    public static void main(String[] args) throws IOException    {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
-        int testCases = Integer.parseInt(br.readLine());
 
-        for(int k = 0; k < testCases; k++) {
-            worms = 0;
+        st = new StringTokenizer(br.readLine());
+        int rowCases = Integer.parseInt(st.nextToken());
+        int columnCases = Integer.parseInt(st.nextToken());
+        int squareCases = Integer.parseInt(st.nextToken());
 
+        map = new int[rowCases][columnCases];
+        visited = new boolean[rowCases][columnCases];
+        countSizeList = new ArrayList<>();
+
+        //직사각형 map에 초기화
+        for(int i = 0; i < squareCases; i++) {
             st = new StringTokenizer(br.readLine());
-            int row = Integer.parseInt(st.nextToken());
-            int column = Integer.parseInt(st.nextToken());
-            int cabbageCount = Integer.parseInt(st.nextToken());
 
-            map = new int[column][row];
-            visit = new boolean[column][row];
-            que = new LinkedList<>();
+            int startX = Integer.parseInt(st.nextToken());
+            int startY = Integer.parseInt(st.nextToken());
+            int endX = Integer.parseInt(st.nextToken());
+            int endY = Integer.parseInt(st.nextToken());
 
-            for (int i = 0; i < cabbageCount; i++) {
-                st = new StringTokenizer(br.readLine());
-                int x = Integer.parseInt(st.nextToken());
-                int y = Integer.parseInt(st.nextToken());
-                map[y][x] = 1;
-            }
-
-            for (int i = 0; i < map.length; i++) {
-                for (int j = 0; j < map[0].length; j++) {
-                    if (!visit[i][j] && map[i][j] == 1) {
-                        que.offer(new Node(j, i));
-                        visit[i][j] = true;
-                        bfs(i, j);
-                    }
+            for(int j = startY; j < endY; j++) {
+                for(int k = startX; k < endX; k++) {
+                    map[j][k] = 1;
                 }
             }
-            sb.append(worms).append("\n");
+
         }
+
+        for(int i = 0; i < map.length; i++) {
+            for(int j = 0; j < map[i].length; j++) {
+                if(map[i][j] == 1 && !visited[i][j]) {
+                    countSize = 0;
+                    bfs(j, i);
+                    countSizeList.add(countSize);
+                }
+            }
+        }
+
+        sb.append(countSizeList.size())
+                .append("\n");
+
+        for(int count : countSizeList) {
+            sb.append(count)
+                    .append(" ");
+        }
+
         System.out.println(sb);
     }
 
-    private static void bfs(int y, int x) {
-        worms++;
+    private static void bfs(int x, int y) {
+        Queue<Node> que = new LinkedList<>();
+        que.offer(new Node(x,y));
+        countSize++;
+        visited[y][x] = true;
 
-        while (!que.isEmpty()) {
+        while(!que.isEmpty()) {
             Node node = que.poll();
 
-            for (int i = 0; i < 4; i++) {
-                curX = node.x + moveX[i];
-                curY = node.y + moveY[i];
+            if(!visited[node.y][node.x]) {
+                visited[node.y][node.x] = true;
+                countSize++;
 
-                if (checkBoundary(curX, curY) && !visit[curY][curX] && map[curY][curX] == 1) {
-                    que.offer(new Node(curX, curY));
-                    visit[curY][curX] = true;
+                for(int i = 0; i < 4; i++) {
+                    int curX = node.x + moveX[i];
+                    int curY = node.y + moveY[i];
+
+                    if(map[curY][curX] == 1 && !visited[curY][curX] && checkBoundary(curX, curY)) {
+                        que.offer(new Node(curX,curY));
+                    }
                 }
             }
 
         }
-    }
 
-    private static boolean checkBoundary(int x, int y) {
-        return x >= 0 && x < map[0].length && y >= 0 && y < map.length;
     }
 
     public static class Node {
@@ -87,4 +100,12 @@ public class Main {
             this.y = y;
         }
     }
+
+    private static boolean checkBoundary(int x, int y) {
+        if(x >= 0 && x < map[0].length && y >= 0 && y < map.length) {
+            return true;
+        }
+            return false;
+    }
+
 }
