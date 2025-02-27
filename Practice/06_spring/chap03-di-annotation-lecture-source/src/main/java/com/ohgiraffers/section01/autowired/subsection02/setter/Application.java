a33a38e612a4bc818978d3e0a3baa3436762d857
Gombo2 <rhtjddus0502@gmail.com>
2025-02-27 14:41:57 +0900

    Field Di 예제

---
+++ Practice/06_spring/chap03-di-annotation-lecture-source/src/main/java/com/ohgiraffers/section01/autowired/subsection02/setter/Application.java
@@ -0,0 +1,20 @@
+package com.ohgiraffers.section01.autowired.subsection02.setter;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Application {
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.ohgiraffers.section01");
+
+        BookService bookService = context.getBean("bookServiceSetter", BookService.class);
+
+        bookService.findAllBook().forEach(System.out::println);
+
+        System.out.println(bookService.findBookBySequence(1));
+        System.out.println(bookService.findBookBySequence(2));
+
+//        System.out.println("bookService = " + bookService);
+    }
+}
