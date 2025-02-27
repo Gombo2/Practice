8e903cc72aa68180c080448b9c3a8334846449c0
Gombo2 <rhtjddus0502@gmail.com>
2025-02-27 09:51:05 +0900

    annotation을 통한 bean factory 생성 및 bean 굽기

--- Practice/06_spring/chap01-bean-factory-lecture-source/src/main/java/com/ohgiraffers/section01/javaconfig/Application.java
+++ Practice/06_spring/chap01-bean-factory-lecture-source/src/main/java/com/ohgiraffers/section01/javaconfig/Application.java
@@ -8,6 +8,7 @@ public class Application {
     public static void main(String[] args) {
 
         /* 설명. 콩을 관리하는 SpringContainer -> ApplicationContext. */
+        // IoC container, bean factory 라고도 한다.
         ApplicationContext context = new AnnotationConfigApplicationContext(ContextConfiguration.class);
 
         String[] beanNames = context.getBeanDefinitionNames();
@@ -16,3 +17,4 @@ public class Application {
         }
     }
 }
+
---
+++ Practice/06_spring/chap01-bean-factory-lecture-source/src/main/java/com/ohgiraffers/section02/xmlconfig/Application.java
@@ -0,0 +1,22 @@
+package com.ohgiraffers.section02.xmlconfig;
+
+import com.ohgiraffers.common.MemberDTO;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class Application {
+    public static void main(String[] args) {
+        ApplicationContext context = new GenericXmlApplicationContext("section02/xmlconfig/spring-context.xml");
+
+
+        /* 설명. 컨테이너에 들어있는 모든 bean의 이름(id)을 확인 */
+        String[] beanNames = context.getBeanDefinitionNames();
+        for(String beanName : beanNames) {
+            System.out.println("beanName = " + beanName);
+        }
+
+        /* 설명. 1. bean의 id를 가지고 추출 */
+//        MemberDTO member = (MemberDTO)context.getBean("member");
+//        System.out.println("member = " + member);
+    }
+}
