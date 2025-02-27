e01f3c91df60a23b0a1dbc1f38fceb28beb8cdff
Gombo2 <rhtjddus0502@gmail.com>
2025-02-27 11:50:14 +0900

    DI 관련 코드 예제
    1. 의존성 주입
    
    2. 대입관계 설정

--- Practice/06_spring/chap02-dependency-injection-lecture-source/src/main/java/com/ohgiraffers/common/MemberDTO.java
+++ Practice/06_spring/chap02-dependency-injection-lecture-source/src/main/java/com/ohgiraffers/common/MemberDTO.java
@@ -11,6 +11,7 @@ public class MemberDTO {
     private int sequence;
     private String name;
     private String email;
+    private String phone;
     private Account personalAccount;            // 개인 계좌
                                                 // interface 객체를 받야아 함으로 의존관계가 형성된다.
 }
