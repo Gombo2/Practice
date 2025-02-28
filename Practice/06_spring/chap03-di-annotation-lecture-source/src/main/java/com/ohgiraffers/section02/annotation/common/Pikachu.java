ac10133be4aac1d60e326fc2440b158c88d054da
Gombo2 <rhtjddus0502@gmail.com>
2025-02-28 09:49:52 +0900

    DI 관련 코드 예제
    1. 의존성 주입
    
    2. 대입관계 설정

---
+++ Practice/06_spring/chap03-di-annotation-lecture-source/src/main/java/com/ohgiraffers/section02/annotation/common/Pikachu.java
@@ -0,0 +1,8 @@
+package com.ohgiraffers.section02.annotation.common;
+
+public class Pikachu implements Pokemon {
+    @Override
+    public void attack() {
+
+    }
+}
---
+++ Practice/06_spring/chap03-di-annotation-lecture-source/src/main/java/com/ohgiraffers/section02/annotation/common/Squirtle.java
@@ -0,0 +1,8 @@
+package com.ohgiraffers.section02.annotation.common;
+
+public class Squirtle implements Pokemon {
+    @Override
+    public void attack() {
+
+    }
+}
