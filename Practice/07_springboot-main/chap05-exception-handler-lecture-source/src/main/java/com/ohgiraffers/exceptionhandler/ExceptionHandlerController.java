a0f02a5a4dcb95fc09790092f75bd02568571841
Gombo2 <rhtjddus0502@gmail.com>
2025-03-06 14:05:19 +0900

    수정

---
+++ Practice/07_springboot-main/chap05-exception-handler-lecture-source/src/main/java/com/ohgiraffers/exceptionhandler/ExceptionHandlerController.java
@@ -0,0 +1,19 @@
+package com.ohgiraffers.exceptionhandler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/*")
+public class ExceptionHandlerController {
+    @GetMapping("simple-null")
+    public String simpleNullPointerExceptionTest() {
+
+        if(true) {
+            throw new NullPointerException();
+        }
+
+        return "/";
+    }
+}
