11e409a28164de8eec76f21e9822b0b82e2506df
Gombo2 <rhtjddus0502@gmail.com>
2025-03-06 09:49:56 +0900

    HttpSession을 활용한 예제

--- Practice/07_springboot-main/chap03-handler-method-lecture-source/src/main/java/com/ohgiraffers/handlermethod/FirstController.java
+++ Practice/07_springboot-main/chap03-handler-method-lecture-source/src/main/java/com/ohgiraffers/handlermethod/FirstController.java
@@ -1,14 +1,19 @@
 package com.ohgiraffers.handlermethod;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 
 import java.util.Map;
 
 @Controller
 @RequestMapping("/first")   // Controller에서 1차적으로 /first @ 에 대한 요청을 받아옴.
+/* 설명. 이 Controller 클래스의 핸드러 메소드에서 Model에 "id"라는 키 값으로 담긴 값은 HttpSession에 추가하는 어노테이션 */
+/* 설명. HttpSession에서 제공하는 invalidate()가 아닌 SessionStatus가 제공하는 setComplete()를 통해 만료시킬 수 있다. */
+@SessionAttributes({"id"})
 public class FirstController {
 
     /* 설명. 핸들러 메소드에서 반환형이 없을 경우 요청경로를 반환한다.(view의 경로 및 이름)*/
@@ -102,5 +107,45 @@ public class FirstController {
         System.out.println("menu = " + menu);
 
         return "first/searchResult";
+    }
+
+    @GetMapping("login")
+    public void login() {}
+
+    @PostMapping("login")
+    /* 설명. @RequestParam 형식으로 받은거(변수명이 같아 생략) + 추가 설정 없을시 기본 30분 지속 session 발생. */
+    public String sessionTest1(String id, String pwd, HttpSession session){
+        System.out.println("id = " + id);
+        System.out.println("pwd = " + pwd);
+
+    /* 설명. 로그인 성공을 가정(회원 조회 이후) HttpSession에 로그인 성공한 회원 정보 저장 */
+        session.setAttribute("id",id);
+        session.setAttribute("pwd",pwd);
+
+
+        return "first/loginResult";
+    }
+
+    @GetMapping("logout1")
+    public String logoutTest1(HttpSession session) {
+        session.invalidate();
+
+        return "first/loginResult";
+    }
+
+    /* 설명. Model에 담은 값 중에 일부를 HttpSession에 자동으로 담도록 어노테이션 활용 */
+    @PostMapping("login2")
+    public String sessionTest2(Model model, String id) {
+        model.addAttribute("id",id);
+
+        return "first/loginResult";
+    }
+
+    @GetMapping("logout2")
+    public String logoutTest2(SessionStatus sessionStatus) {
+        sessionStatus.setComplete();
+        /* 설명. annotation을 통해 활성화된 session은 invalidate로 처리가 안되게 되있어서 SessionStatus를 활용해야 한다.*/
+
+        return "first/loginResult";
     }
 }
