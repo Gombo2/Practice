85bba54a7c6a8821eb6fb41ad475a4f138733c1e
Gombo2 <106304305+Gombo2@users.noreply.github.com>
2025-03-06 14:48:13 +0900

    사용자 정의 Exception

--- Practice/07_springboot-main/chap05-exception-handler-lecture-source/src/main/java/com/ohgiraffers/exceptionhandler/RootConfiguration.java
+++ Practice/07_springboot-main/chap05-exception-handler-lecture-source/src/main/java/com/ohgiraffers/exceptionhandler/RootConfiguration.java
@@ -15,6 +15,7 @@ public class RootConfiguration {
         /* 설명. 예외 타입과 처리할 페이지 설정 */
         Properties props = new Properties();
         props.setProperty("java.lang.NullPointerException", "error/nullPointer");
+        props.setProperty("MemberRegistExcpetion", "error/memberRegist");
 
         /* 설명. 전체 예외 관련되어 설정하기 */
         /* 설명. 1. 예외에 따른 페이지 설정한 것 활용 */
