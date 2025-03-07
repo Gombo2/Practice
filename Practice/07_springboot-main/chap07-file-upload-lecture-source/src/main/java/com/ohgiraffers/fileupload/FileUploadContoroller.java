7757a0c6d5d6f988f8e44bccd2d5e051850eee6e
Gombo2 <106304305+Gombo2@users.noreply.github.com>
2025-03-07 11:51:19 +0900

    파일 업로드 관련 예제

---
+++ Practice/07_springboot-main/chap07-file-upload-lecture-source/src/main/java/com/ohgiraffers/fileupload/FileUploadContoroller.java
@@ -0,0 +1,27 @@
+package com.ohgiraffers.fileupload;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class FileUploadContoroller {
+
+    /* 설명.  multipart/form-data로 넘어오는 값은 MultipartFile로 받아내야 한다. */
+    @PostMapping("single-file")
+    public String singleFile(@RequestParam MultipartFile singleFile,
+                             @RequestParam String singleFileDescription) {
+
+        System.out.println("singleFile = " + singleFile);
+        System.out.println("singleFileDescription = " + singleFileDescription);
+
+        return "redirect:/result";
+    }
+
+    @GetMapping("result")
+    public void result() {}
+
+}
