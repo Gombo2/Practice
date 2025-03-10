228486d11f3c7bb28b3f0d6f4231c2f53ffe6765
Gombo2 <106304305+Gombo2@users.noreply.github.com>
2025-03-10 09:50:00 +0900

    Mybatis connection 설정 관련 예시
    jdbc 관련 확인후 공부

---
+++ Practice/08_mybatis/chap01-connection-config-lecture/src/main/java/com/ohgiraffers/section01/javaconfig/Application.java
@@ -0,0 +1,8 @@
+package com.ohgiraffers.section01.javaconfig;
+
+public class Application {
+    private static String dirver = "com.mysql.cj.jdbc.Driver";
+    private static String url = "jdbc:mysql://localhost:3306/menudb";
+    private static String user = "practice";
+    private static String password = "practice";
+}
--- Practice/Algorithm/src/main/java/Main.java
+++ Practice/Algorithm/src/main/java/Main.java
@@ -5,107 +5,50 @@ import java.util.*;
 
 
 public class Main {
-    static int[][] map;
-    static boolean[][] visited;
-    static int countSize;
-    static List<Integer> countSizeList;
-    static int[] moveX = {0, 0, -1, 1};
-    static int[] moveY = {1, -1, 0, 0};
+    static int minSecond = Integer.MAX_VALUE;
+    static int maxLoc = 100000;
+    static boolean[] visited;
+    static int startPoint;
+    static int endPoint;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        st = new StringTokenizer(br.readLine());
-        int rowCases = Integer.parseInt(st.nextToken());
-        int columnCases = Integer.parseInt(st.nextToken());
-        int squareCases = Integer.parseInt(st.nextToken());
+        startPoint = Integer.parseInt(st.nextToken());
+        endPoint = Integer.parseInt(st.nextToken());
+        visited = new boolean[maxLoc + 1];
 
-        map = new int[rowCases][columnCases];
-        visited = new boolean[rowCases][columnCases];
-        countSizeList = new ArrayList<>();
+        bfs();
 
-        //직사각형 map에 초기화
-        for(int i = 0; i < squareCases; i++) {
-            st = new StringTokenizer(br.readLine());
 
-            int startX = Integer.parseInt(st.nextToken());
-            int startY = Integer.parseInt(st.nextToken());
-            int endX = Integer.parseInt(st.nextToken());
-            int endY = Integer.parseInt(st.nextToken());
-
-            for(int j = startY; j < endY; j++) {
-                for(int k = startX; k < endX; k++) {
-                    map[j][k] = 1;
-                }
-            }
-
-        }
-
-        for(int i = 0; i < map.length; i++) {
-            for(int j = 0; j < map[i].length; j++) {
-                if(map[i][j] == 1 && !visited[i][j]) {
-                    countSize = 0;
-                    bfs(j, i);
-                    countSizeList.add(countSize);
-                }
-            }
-        }
-
-        sb.append(countSizeList.size())
-                .append("\n");
-
-        for(int count : countSizeList) {
-            sb.append(count)
-                    .append(" ");
-        }
-
-        System.out.println(sb);
     }
 
-    private static void bfs(int x, int y) {
+    private static void bfs() {
         Queue<Node> que = new LinkedList<>();
-        que.offer(new Node(x,y));
-        countSize++;
-        visited[y][x] = true;
+        que.offer(new Node(startPoint, 0));
 
         while(!que.isEmpty()) {
             Node node = que.poll();
+            visited[node.x] = true;
 
-            if(!visited[node.y][node.x]) {
-                visited[node.y][node.x] = true;
-                countSize++;
-
-                for(int i = 0; i < 4; i++) {
-                    int curX = node.x + moveX[i];
-                    int curY = node.y + moveY[i];
-
-                    if(map[curY][curX] == 1 && !visited[curY][curX] && checkBoundary(curX, curY)) {
-                        que.offer(new Node(curX,curY));
-                    }
-                }
+            if(node.x == endPoint) {
+                minSecond = Math.min(minSecond, node.second);
             }
 
-        }
+            if(node.x <= maxLoc && )
 
+        }
     }
 
-    public static class Node {
+    private static class Node {
         int x;
-        int y;
+        int second;
 
-        public Node(int x, int y) {
+        public Node(int x, int second) {
             this.x = x;
-            this.y = y;
+            this.second = second;
         }
     }
-
-    private static boolean checkBoundary(int x, int y) {
-        if(x >= 0 && x < map[0].length && y >= 0 && y < map.length) {
-            return true;
-        }
-            return false;
-    }
-
 }
