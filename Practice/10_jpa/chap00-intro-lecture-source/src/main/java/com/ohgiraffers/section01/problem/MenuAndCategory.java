9696cad0ad464e6df9db334a45715e7f70bc561a
Gombo2 <106304305+Gombo2@users.noreply.github.com>
2025-03-13 11:30:18 +0900

    JPA

--- Practice/10_jpa/chap00-intro-lecture-source/src/main/java/com/ohgiraffers/section01/problem/MenuAndCategory.java
+++ Practice/10_jpa/chap00-intro-lecture-source/src/main/java/com/ohgiraffers/section01/problem/MenuAndCategory.java
@@ -4,55 +4,54 @@ public class MenuAndCategory {
     private int menuCode;
     private String menuName;
     private int menuPrice;
-    @ManyToOne
     private Category category;
     private String orderableStatus;
 
     public MenuAndCategory() {
     }
 
-    public MenuAndCategory(int menuCode, String menuName, int menuPrice, Category category, String oderableStatus) {
+    public MenuAndCategory(int menuCode, String menuName, int menuPrice, Category category, String orderableStatus) {
         this.menuCode = menuCode;
         this.menuName = menuName;
         this.menuPrice = menuPrice;
         this.category = category;
-        this.oderableStatus = oderableStatus;
+        this.orderableStatus = orderableStatus;
     }
 
     public int getMenuCode() {
         return menuCode;
     }
 
-    public String getMenuName() {
-        return menuName;
-    }
-
-    public int getMenuPrice() {
-        return menuPrice;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public String getOrderableStatus() {
-        return orderableStatus;
-    }
-
     public void setMenuCode(int menuCode) {
         this.menuCode = menuCode;
+    }
+
+    public String getMenuName() {
+        return menuName;
     }
 
     public void setMenuName(String menuName) {
         this.menuName = menuName;
     }
 
+    public int getMenuPrice() {
+        return menuPrice;
+    }
+
     public void setMenuPrice(int menuPrice) {
         this.menuPrice = menuPrice;
     }
 
+    public Category getCategory() {
+        return category;
+    }
+
     public void setCategory(Category category) {
         this.category = category;
+    }
+
+    public String getOrderableStatus() {
+        return orderableStatus;
     }
 
     public void setOrderableStatus(String orderableStatus) {
@@ -66,9 +65,7 @@ public class MenuAndCategory {
                 ", menuName='" + menuName + '\'' +
                 ", menuPrice=" + menuPrice +
                 ", category=" + category +
-                ", oderableStatus='" + oderableStatus + '\'' +
+                ", orderableStatus='" + orderableStatus + '\'' +
                 '}';
     }
-
-
 }
