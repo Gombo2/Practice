1a1e4298a16a3ca9f9b9cdb98607c3a0cfe23d62
Gombo2 <106304305+Gombo2@users.noreply.github.com>
2025-03-19 14:25:40 +0900

    feat: Gateway를 통한 전처리 후처리 적용 first-service에 대해

--- Practice/11_msa/chap02-01-first-service-lecture-source/src/main/java/com/ohgiraffers/firstservice/FirstServiceController.java
+++ Practice/11_msa/chap02-01-first-service-lecture-source/src/main/java/com/ohgiraffers/firstservice/FirstServiceController.java
@@ -3,6 +3,7 @@ package com.ohgiraffers.firstservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +26,12 @@ public class FirstServiceController {
 
 		/* 설명. Environment를 활용해서 local.server.port를 가져오면 동적으로 할당된 실제 포트번호 확인 가능 */
 		return "I'm OK, 포트는 " + env.getProperty("local.server.port");
+	}
+
+	@GetMapping("/message")
+	public String message(@RequestHeader("first-request") String header) {
+		log.info("넘어온 헤더값: {}", header);
+
+		return "First Service Message";
 	}
 }
