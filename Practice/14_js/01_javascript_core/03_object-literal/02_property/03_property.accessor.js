3bfab565faa892ee341ff7d9d0b99b391965b03f
Gombo2 <106304305+Gombo2@users.noreply.github.com>
2025-04-01 10:52:01 +0900

    feat : Java Script 함수 관련 기능 추가

--- Practice/14_js/01_javascript_core/03_object-literal/02_property/03_property.accessor.js
+++ Practice/14_js/01_javascript_core/03_object-literal/02_property/03_property.accessor.js
@@ -13,3 +13,21 @@ dog.eat('감자');
 /* 2. 대괄호 표기법(square braket notation) */
 console.log(dog['name']); // 뽀삐
 dog['eat']('고구마');
+
+/* 3. 대괄호 표기법을 특별히 고려해야 하는 경우 */
+var obj = {
+    under_key: 'under_key', // 프로퍼티키를 문자열이 아닌 형태로 할 수 있는건 _ , $ 만 가능.
+    'dash-key': 'dash-key',   // 대쉬(-)는 사용할 수 없다. 사용하려면 ' ' 사용해야
+    0: 1
+};
+
+/* 마침표 표기법은 접근에 한계가 있다. */
+// console.log(obj.under_key);
+// console.log(objdash-key);
+// console.log(obj.0);
+
+/* 대괄호 표기법은 다양한 형태의 프로퍼티들을 모두 접근할 수 있다. */
+console.log(obj['under_key']); // under_key
+console.log(obj['dash-key']); // dash-key
+console.log(obj['0']); // 1
+console.log(obj[0]); // 숫자 프로퍼티 키일 경우에는 숫자로 바로 접근 가능.
---
+++ Practice/14_js/01_javascript_core/04_function/01_function_definition/01_define-function.js
@@ -0,0 +1,18 @@
+// 기본 선언 방식 (함수 선언문 형태)
+function hello(name) {
+    return `${name}님 안녕하세요!`;
+};
+
+console.log(hello('홍길동'));
+
+//함수를 하나의 값으로 생각. (함수 표현식 형태)
+/*
+  자바 스크립트의 함수는 객체 타입의 값으로 값의 성질을 가지는 객체로써 일급 객체라고 한다
+  따라서 함수 리터럴로 생성한 함수 객체를 변수에 할당할 수 있다.
+*/
+
+var hello2 = function(name) {
+    return `${name}님 반갑습니다!`;
+};
+
+console.log(hello('강감찬'));
