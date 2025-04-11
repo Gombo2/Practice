8e91988221000e0f7d980c6d2e051d6efeb50cc7
Gombo2 <106304305+Gombo2@users.noreply.github.com>
2025-04-11 10:17:13 +0900

    feat : cors 해결 후 계산값 반환환

--- Practice/16_devops/chap01_02_vueproject/src/App.vue
+++ Practice/16_devops/chap01_02_vueproject/src/App.vue
@@ -4,6 +4,8 @@
         <label>num: 1</label><input type="text" v-model="num1">&nbsp;
         <label>num: 2</label><input type="text" v-model="num2">&nbsp;
         <button @click="sendPlus">더하기</button>
+        <hr>
+        <p>{{ num1 }} + {{ num2 }} = {{ sum }}</p>
     </div>
 </template>
 
@@ -12,14 +14,18 @@
 
     const num1 = ref(0);
     const num2 = ref(0);
+    const sum = ref(0);
 
     const sendPlus = async() => {
         const response = await fetch(`http://localhost:7777/plus?num1=${num1.value}&num2=${num2.value}`);
         const data = await response.json();
         console.log('data: ', data);
+        sum.value = data.sum;
     }
 </script>
 
 <style scoped>
-
+    .plus {
+        text-align: center;
+    }
 </style>
