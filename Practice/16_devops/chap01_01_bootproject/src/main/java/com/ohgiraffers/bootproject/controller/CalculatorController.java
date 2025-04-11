1e55b8daf63ff43044f3949b799854e680f501a3
Gombo2 <106304305+Gombo2@users.noreply.github.com>
2025-04-11 09:49:16 +0900

    feat: 변경경

--- Practice/16_devops/chap01_01_bootproject/src/main/java/com/ohgiraffers/bootproject/controller/CalculatorController.java
+++ Practice/16_devops/chap01_01_bootproject/src/main/java/com/ohgiraffers/bootproject/controller/CalculatorController.java
@@ -1,4 +1,29 @@
 package com.ohgiraffers.bootproject.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ohgiraffers.bootproject.dto.CalculatorDTO;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
 public class CalculatorController {
+
+	@GetMapping("/health")
+	public String healthCheck() {
+		return "I'm alive!!";
+	}
+
+	//커맨드 객체로 프론트걸 받을거임 -> DTO로 한번에 받기
+	//fetch의 결과로 promise에 담길 성공 결과가 response~DTO. 뒤의 DTO는  num1,num2받을 용도
+	@GetMapping("/plus")
+	public ResponseEntity<CalculatorDTO> plusTwoNumbers(CalculatorDTO calculatorDTO) {
+
+		log.info("calculatorDTO: {}", calculatorDTO);
+
+		return ResponseEntity.ok().body(null);
+	}
 }
---
+++ Practice/16_devops/chap01_02_vueproject/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="plus">
+        <h1>덧셈 기능 만들기</h1>
+        <label>num: 1</label><input type="text" v-model="num1">&nbsp;
+        <label>num: 2</label><input type="text" v-model="num2">&nbsp;
+        <button @click="sendPlus">더하기</button>
+    </div>
+</template>
+
+<script setup>
+    import { ref } from 'vue';
+
+    const num1 = ref(0);
+    const num2 = ref(0);
+
+    const sendPlus = async() => {
+        const response = await fetch(`http://localhost:7777/plus?num1=${num1.value}&num2=${num2.value}`);
+        const data = await response.json();
+        console.log('data: ', data);
+    }
+</script>
+
+<style scoped>
+
+</style>
